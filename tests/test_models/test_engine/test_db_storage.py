bfc9b523ef2bd232f93656c73313ea8d1b453792
Adeeyo Michael <adeeyomichael77@gmail.com>
2023-08-01 06:09:29 -0700

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,59 +1,29 @@
+#!/usr/bin/python3
+""" Test module for the Database storage"""
+
 import unittest
+import pycodestyle
 from models.engine.db_storage import DBStorage
-from models.state import State
 
 
 class TestDBStorage(unittest.TestCase):
-    def setUp(self):
-        self.db = DBStorage()
-        self.db.reload()
+    """ Clas TestDBStorage for testing the database storage"""
 
-    def tearDown(self):
-        self.db.close()
+    def testPycodeStyle(self):
+        """Test for pycodestyle compliancy in DBStorage"""
+        style = pycodestyle.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_all_method_returns_dictionary(self):
-        result = self.db.all()
-        self.assertIsInstance(result, dict)
-
-    def test_all_method_returns_dictionary_for_state(self):
-        result = self.db.all(State)
-        for key, value in result.items():
-            self.assertIsInstance(value, State)
-
-    def test_new_method_adds_to_session(self):
-        state = State(name="New York")
-        self.db.new(state)
-        key = "{}.{}".format(type(state).__name__, state.id)
-        self.assertIn(key, self.db._DBStorage__session.new)
-
-    def test_save_method_saves_changes(self):
-        state = State(name="California")
-        self.db.new(state)
-        self.db.save()
-        self.assertIn(state, self.db._DBStorage__session)
-
-    def test_delete_method_deletes_from_session(self):
-        state = State(name="Florida")
-        self.db.new(state)
-        self.db.save()
-        self.db.delete(state)
-        key = "{}.{}".format(type(state).__name__, state.id)
-        self.assertNotIn(key, self.db._DBStorage__session)
-
-    def test_get_method_returns_object_by_id(self):
-        state = State(name="Texas")
-        self.db.new(state)
-        self.db.save()
-        retrieved_state = self.db.get("State", state.id)
-        self.assertEqual(state, retrieved_state)
-
-    def test_count_method_counts_objects(self):
-        count = self.db.count()
-        self.assertEqual(count, len(self.db.all()))
-
-    def test_count_method_counts_objects_of_specified_class(self):
-        count = self.db.count("State")
-        self.assertEqual(count, len(self.db.all("State")))
+    def test_docstring_DBStorage(self):
+        """Test for docstring in DBStorage"""
+        self.assertIsNotNone(DBStorage.__doc__)
+        self.assertIsNotNone(DBStorage.__init__.__doc__)
+        self.assertIsNotNone(DBStorage.all.__doc__)
+        self.assertIsNotNone(DBStorage.new.__doc__)
+        self.assertIsNotNone(DBStorage.save.__doc__)
+        self.assertIsNotNone(DBStorage.delete.__doc__)
+        self.assertIsNotNone(DBStorage.reload.__doc__)
 
 
 if __name__ == "__main__":
